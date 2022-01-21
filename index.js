9873f853fdc6a67619cedce51798beeb90581a4d
vitorcarneiro <vitorcarneiro.sdo@gmail.com>
2022-01-21 12:21:32 -0300

    feat: tweets working

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-import express from 'express';
+import express, {json} from 'express';
 import cors from 'cors';
 
 const user = {
 	username: '', 
 	avatar: '' 
 }
+
+let tweets = [
+    {
+	    username: "driven",
+        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+        tweet: "eu amo o hub"
+    },
+    {
+	    username: "estudantes_drivens",
+        avatar: 'https://www.impactonoticias.com.br/wp-content/uploads/2020/07/10-de-julho-de-2020.jpg',
+        tweet: "odiamos o hub"
+    },
+    {
+	    username: "fausto",
+        avatar: 'https://istoe.com.br/wp-content/uploads/sites/14/2022/01/faustao-credito-da-foto-reproducao-band.jpg',
+        tweet: "o loco meu"
+    },
+];
 
 const server = express();
 server.use(cors(), express.json());
@@ -22,7 +40,7 @@ server.post('/tweets', (req, res) => {
 });
 
 server.get('/tweets', (req, res) => {
-    res.send("OK");
+    res.send(tweets);
 });
 
 server.listen(5000, () => {
